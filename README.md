24cb5733f13f43eea7a462c8b161199b65d992d7
krabhi1 <abhishek33gl@gmail.com>
2025-05-20 17:35:17 +0530

    Add initial project setup with main application logic and dependencies

---
+++ README.md
@@ -0,0 +1,12 @@
+# livekit-worker-urbanchat
+
+### Commands
+```bash
+#python venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python main.py  download-files
+# run the worker
+python main.py console|dev|start
+```
---
+++ main.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions,RoomOutputOptions
+from livekit.plugins import (
+    openai,
+    elevenlabs,
+    deepgram,
+    noise_cancellation,
+    silero,
+)
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+load_dotenv()
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions="You are a helpful voice AI assistant.")
+
+
+async def entrypoint(ctx: agents.JobContext):
+    await ctx.connect()
+
+    session = AgentSession(
+        stt=deepgram.STT(model="nova-3", language="multi"),
+        llm=openai.LLM(model="gpt-4o-mini"),
+        tts=deepgram.TTS(),
+        vad=silero.VAD.load(),
+        turn_detection=MultilingualModel(),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            noise_cancellation=noise_cancellation.BVC(),
+            text_enabled=True,
+            audio_enabled=True,
+
+        ),
+        room_output_options=RoomOutputOptions(
+            transcription_enabled=True,
+            audio_enabled=True,
+        ),
+    )
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
